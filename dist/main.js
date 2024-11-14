70a125bce4100ed9270ee83aee617d0303b803c9
EsleySaab <esleysantana.sa@gmail.com>
2024-11-14 18:56:34 -0300

    using webpack server

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("document.addEventListener(\"DOMContentLoaded\", function () {\n  const modal = document.getElementById(\"myModal\")\n  const button = document.getElementById(\"modal-button\")\n  const closeButton = document.getElementsByClassName(\"close\")[0]\n\n  button.onclick = function () {\n    modal.style.display = \"flex\"\n  }\n\n  closeButton.onclick = function () {\n    modal.style.display = \"none\"\n  }\n})\n\n\n//# sourceURL=webpack://mundopet/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/main.js"]();
+/******/ 	
+/******/ })()
+;
